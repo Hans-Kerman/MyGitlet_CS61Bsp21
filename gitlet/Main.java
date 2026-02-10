5503283f6aca129cccb0f345e1a90eb350fed5d4
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-02-10 17:26:54 +0800

    debug:补充输入命令不对、参数不对等情况在Main.java中缺漏的error message

--- gitlet/Main.java
+++ gitlet/Main.java
@@ -12,42 +12,73 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO: what if args is empty?
+        if (args.length == 0) {
+            message("Please enter a command.");
+            System.exit(0);
+        }
         String firstArg = args[0];
+        if (!firstArg.equals("init")) {
+            repoExistCheck();
+        }
         try {
             switch (firstArg) {
                 case "init":
                     // TODO: handle the `init` command
+                    if (args.length != 1) {
+                        badCommandExit();
+                    }
                     Repository.InitRepository();
                     break;
                 case "add":
                     // TODO: handle the `add [filename]` command
+                    if (args.length != 2) {
+                        badCommandExit();
+                    }
                     String fileName = args[1];
                     Repository.addFileToStage(fileName);
                     break;
                 // TODO: FILL THE REST IN
                 case "commit":
+                    if (args.length != 2) {
+                        badCommandExit();
+                    }
                     // 基础错误处理：没message
-                    if (args.length < 2 || args[1].isBlank()) {
+                    if (args[1].isBlank()) {
                         throw error("Please enter a commit message.");
                     } else {
                         Repository.commitToGitlet(args[1]);
                     }
                     break;
                 case "rm":
+                    if (args.length != 2) {
+                        badCommandExit();
+                    }
                     String filename = args[1];
                     Repository.removeFile(filename);
                     break;
                 case "log":
+                    if (args.length != 1) {
+                        badCommandExit();
+                    }
                     Repository.printLog();
                     break;
                 case "global-log":
+                    if (args.length != 1) {
+                        badCommandExit();
+                    }
                     Repository.printGlobalLog();
                     break;
                 case "find":
+                    if (args.length != 2) {
+                        badCommandExit();
+                    }
                     String matchMessage = args[1];
                     Repository.findMatchMessage(matchMessage);
                     break;
                 case "status":
+                    if (args.length != 1) {
+                        badCommandExit();
+                    }
                     Repository.printStatus();
                     break;
                 case "checkout":
@@ -57,30 +88,52 @@ public class Main {
                         Repository.checkoutOneCommitFile(args[1], args[3]);
                     } else if (args.length == 2) {
                         Repository.checkoutWholeBranch(args[1]);
+                    } else {
+                        badCommandExit();
                     }
                     break;
                 case "branch":
-                    if (args.length == 2) {
-                        Repository.makeBranch(args[1]);
-                    }break;
-                case "rm-branch":
-                    if (args.length == 2) {
-                        Repository.removeBranch(args[1]);
+                    if (args.length != 2) {
+                        badCommandExit();
                     }
+                    Repository.makeBranch(args[1]);
+                    break;
+                case "rm-branch":
+                    if (args.length != 2) {
+                        badCommandExit();
+                    }
+                    Repository.removeBranch(args[1]);
                     break;
                 case "reset":
-                    if (args.length == 2) {
-                        Repository.reset(args[1]);
+                    if (args.length != 2) {
+                        badCommandExit();
                     }
+                    Repository.reset(args[1]);
                     break;
                 case "merge":
-                    if (args.length == 2) {
-                        Repository.merge(args[1]);
+                    if (args.length != 2) {
+                        badCommandExit();
                     }
+                    Repository.merge(args[1]);
                     break;
+                default:
+                    message("No command with that name exists.");
             }
         } catch (GitletException e) {
             System.out.println(e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    private static void badCommandExit() {
+        message("Incorrect operands.");
+        System.exit(0);
+    }
+
+    private static void repoExistCheck() {
+        if (!join(Repository.CWD, ".gitlet").exists()) {
+            message("Not in an initialized Gitlet directory.");
+            System.exit(0);
         }
     }
 }
